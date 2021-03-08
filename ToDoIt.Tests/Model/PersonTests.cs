2328ce28224ad3f5dff2f05d8e595178e96964ae
Richard Krejstrup <krejstrup@hotmail.com>
2021-03-08 13:55:23 +0100

    Classes Person done and tested

--- ToDoIt.Tests/Model/PersonTests.cs
+++ ToDoIt.Tests/Model/PersonTests.cs
@@ -1,14 +1,68 @@
 using System;
 using Xunit;
+using ToDoIt.Model;
 
 namespace ToDoIt.Tests
 {
     public class PersonTests
     {
         [Fact]
-        public void Test1()
+        public void PersonCreateValidNames()
         {
+            int myPersonID = 0;
+            string myFirstName = "Bosse";
+            string myLastName = "Larsson";
+            string myNameResult = "Bosse Larsson";
 
+            Person myPerson = new Person(myPersonID, myFirstName, myLastName);
+
+            // Assert
+            Assert.Equal(myNameResult , myPerson.Name);
+        }
+    
+
+
+        [Fact]
+        public void PersonCreateInValidNames()
+        {
+            int myPersonID = 0;
+            string myFirstName = "";
+            string myLastName = "Larsson";
+            string myNameResult = "John Larsson";
+
+            Person myPerson = new Person(myPersonID, myFirstName, myLastName);
+
+            // Assert
+            Assert.Equal(myNameResult, myPerson.Name);
+        }
+
+        [Fact]
+        public void PersonCreateNullLastName()
+        {
+            int myPersonID = 0;
+            string myFirstName = "Bosse";
+            string myLastName = null;
+            string myNameResult = "Bosse Doe";
+
+            Person myPerson = new Person(myPersonID, myFirstName, myLastName);
+
+            // Assert
+            Assert.Equal(myNameResult, myPerson.Name);
+        }
+
+        [Fact]
+        public void PersonCreateID()
+        {
+            int myPersonID = 0;
+            string myFirstName = "Bosse";
+            string myLastName = "Larsson";
+            int myIdResult = 0;
+         
+
+            Person myPerson = new Person(myPersonID, myFirstName, myLastName);
+
+            // Assert
+            Assert.Equal(myIdResult, myPerson.PersonId);
         }
     }
 }
--- ToDoIt/Model/Person.cs
+++ ToDoIt/Model/Person.cs
@@ -4,7 +4,47 @@ using System.Text;
 
 namespace ToDoIt.Model
 {
-    class Person
+    public class Person
     {
+        private readonly int personId;
+        private string firstName;
+        private string lastName;
+
+        public Person(int myPersonId, string myFirstName, string myLastName)
+        {
+            personId = myPersonId; 
+
+            // have to check for Null or enpty strings
+            if (myFirstName != null && myFirstName.Length > 0)
+            {
+                firstName = myFirstName;
+            }
+            else
+            {
+                firstName = "John";
+            }
+            
+            // have to check for Null or enpty strings
+            if (myLastName != null && myLastName.Length > 0)
+            {
+                lastName = myLastName;
+            }
+            else
+            {
+                lastName = "Doe";
+            };
+
+            
+        }
+
+        public string Name {
+            get { return firstName + " " + lastName; }
+        }
+
+        public int PersonId {
+            get { return personId;  }
+        }
+
     }
 }
+
